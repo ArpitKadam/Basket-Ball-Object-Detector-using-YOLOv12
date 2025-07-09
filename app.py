41a0459cbfde814b0df11f53f61604f794726da6
Arpit Kadam <arpitkadam922@gmail.com>
2025-07-10 00:38:05 +0530

    minor change

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def download_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
